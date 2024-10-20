0fc1949b5855c902187a3db67b33d337730aa644
dhorvatek21 <domagoj.horvatek2@gmail.com>
2024-10-20 15:36:08 +0200

    vjezba2

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+// PHP blok počinje na početku dokumenta prije deklaracije!
+$title = "Da Vincijev kod";
+$link = "https://hr.wikipedia.org/wiki/Da_Vincijev_kod";
+?>
+
+<!DOCTYPE html>
+<html lang="hr">
+<head>
+    <title><?php echo $title; ?></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Da Vincijev kod">
+    <meta name="keywords" content="Da Vinci, kod, triler, knjiga, Dan Brown">
+</head>
+<body>
+    <h1><?php echo $title; ?></h1>
+    <p>Da Vincijev kod je kriminalistički triler američkog pisca Dana Browna.</p>
+    <p>
+        <a href="<?php echo $link; ?>" target="_blank">
+            Poveznica.. 
+        </a>
+    </p>
+    
+    
+    <?php
+    // vjezba2
+    ?>
+    
+</body>
+</html>
